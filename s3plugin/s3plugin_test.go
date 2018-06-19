a9c929464f031caf41a77caca15e3182aab9346d
Chris Hajas <chajas@pivotal.io>
2018-06-18 17:24:19 -0700

    Add support for using custom endpoint
    The S3 plugin now supports specifying a custom endpoint instead of an
    AWS region to backup/restore to/from a storage solution that implements
    the S3 protocol.
    
    Additionally, validate the configuration file prior to attempting to
    connect to the S3 server.
    
    Co-authored-by: Chris Hajas <chajas@pivotal.io>
    Co-authored-by: Amil Khanzada <akhanzada@pivotal.io>

--- s3plugin/s3plugin_test.go
+++ s3plugin/s3plugin_test.go
@@ -23,5 +23,66 @@ var _ = Describe("s3_plugin tests", func() {
 			Expect(newPath).To(Equal(expectedPath))
 		})
 	})
+	Context("ValidateConfig", func() {
+		var pluginConfig *s3plugin.PluginConfig
+		BeforeEach(func() {
+			pluginConfig = &s3plugin.PluginConfig{
+				ExecutablePath: "/tmp/location",
+				Options: map[string]string{
+					"aws_access_key_id":     "12345",
+					"aws_secret_access_key": "6789",
+					"bucket":                "bucket_name",
+					"folder":                "folder_name",
+					"region":                "region_name",
+					"endpoint":              "endpoint_name",
+				},
+			}
+		})
+		It("succeeds when all fields in config filled", func() {
+			err := s3plugin.ValidateConfig(pluginConfig)
+			Expect(err).To(BeNil())
+		})
+		It("succeeds when all fields except endpoint filled in config", func() {
+			delete(pluginConfig.Options, "endpoint")
+			err := s3plugin.ValidateConfig(pluginConfig)
+			Expect(err).To(BeNil())
+		})
+		It("succeeds when all fields except region filled in config", func() {
+			delete(pluginConfig.Options, "region")
+			err := s3plugin.ValidateConfig(pluginConfig)
+			Expect(err).To(BeNil())
+		})
+		It("sets region to unused when endpoint is used instead of region", func() {
+			delete(pluginConfig.Options, "region")
+			s3plugin.ValidateConfig(pluginConfig)
+			Expect(pluginConfig.Options["region"]).To(Equal("unused"))
+		})
+		It("returns error when neither region nor endpoint in config", func() {
+			delete(pluginConfig.Options, "region")
+			delete(pluginConfig.Options, "endpoint")
+			err := s3plugin.ValidateConfig(pluginConfig)
+			Expect(err).To(HaveOccurred())
+		})
+		It("returns error when no aws_access_key_id in config", func() {
+			delete(pluginConfig.Options, "aws_access_key_id")
+			err := s3plugin.ValidateConfig(pluginConfig)
+			Expect(err).To(HaveOccurred())
+		})
+		It("returns error when no aws_secret_access_key in config", func() {
+			delete(pluginConfig.Options, "aws_secret_access_key")
+			err := s3plugin.ValidateConfig(pluginConfig)
+			Expect(err).To(HaveOccurred())
+		})
+		It("returns error when no bucket in config", func() {
+			delete(pluginConfig.Options, "bucket")
+			err := s3plugin.ValidateConfig(pluginConfig)
+			Expect(err).To(HaveOccurred())
+		})
+		It("returns error when no folder in config", func() {
+			delete(pluginConfig.Options, "folder")
+			err := s3plugin.ValidateConfig(pluginConfig)
+			Expect(err).To(HaveOccurred())
+		})
+	})
 
 })
